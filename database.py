37de8c0f261438542795368311857accda0bc388
Jezz <gregege2@mail.ru>
2025-10-18 05:57:42 +0300

    7

--- database.py
+++ database.py
@@ -5,14 +5,14 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:root@localhost:5432/KeyboardMaster'
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:root@localhost:5432/KeyboardMaster'
 
 
 
-# DATABASE_URL = os.getenv("DATABASE_URL")
-#
-# engine = create_engine(DATABASE_URL)
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autoflush=False, bind=engine)
 ModelORM = declarative_base()
 meta = MetaData()
